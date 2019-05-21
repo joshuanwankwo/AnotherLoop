a1e7f247b7de65e4d01b186fb71a8d879c9f21ac
Systemblack007 <systemblakes@gmail.com>
2019-05-21 16:07:35 +0100

    AnotherLoop

---
+++ anotherLoop.js
@@ -0,0 +1,9 @@
+function anotherOne(){
+    for(let i = 1; i <= 12; i++){
+        for(let j = 1; j <= 12; j++){
+            console.log(i + " X " + j + " = "+ i*j)
+        }
+    }
+}
+
+anotherOne();
